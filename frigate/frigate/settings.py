cdf0901f0df1a5dfc318c4641679fb9f7ccaed50
Ghost <yasinabdurakhmanov@gmail.com>
2021-12-07 12:55:32 +0300

    changes

--- frigate/frigate/settings.py
+++ frigate/frigate/settings.py
@@ -13,7 +13,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'p!4c0!-z$u7bi26lr6vll-bf*_mkl=wzd!zd=!sd*=g(1w&jr+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
