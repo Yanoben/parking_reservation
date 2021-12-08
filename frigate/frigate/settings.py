5a8ba81f4069db83ae02ac79126de5afbe34a999
Ghost <yasinabdurakhmanov@gmail.com>
2021-12-08 12:21:42 +0300

    changes model forms setting added templates static

--- frigate/frigate/settings.py
+++ frigate/frigate/settings.py
@@ -59,6 +59,7 @@ TEMPLATES = [
     },
 ]
 
+
 WSGI_APPLICATION = 'frigate.wsgi.application'
 
 
@@ -108,5 +109,15 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
+LOGIN_REDIRECT_URL = '/dashboard/'
+
+LOGIN_URL = 'login'
+LOGOUT_URL = 'logout'
