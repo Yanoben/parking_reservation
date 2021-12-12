c3aa9b9fabf3b00b370cd48b8d8c01fef5cdc4fc
Ghost <yasinabdurakhmanov@gmail.com>
2021-12-12 14:52:00 +0300

    changes

--- frigate/frigate/settings.py
+++ frigate/frigate/settings.py
@@ -127,13 +127,6 @@ AUTH_USER_MODEL = 'users.MyUser'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
-
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 LOGOUT_URL = 'logout'
 
